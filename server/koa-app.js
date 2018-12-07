c72a73a780d2361ebf70e3653ed3523cf3d1ea61
julien.waddle@camptocamp.com <julien.waddle@camptocamp.com>
2018-12-07 16:27:41 +0100

    Add the server

---
+++ server/koa-app.js
@@ -0,0 +1,24 @@
+const Koa = require('koa');
+var Router = require('koa-router');
+
+const app = new Koa();
+const PORT = 8080;
+var router = new Router();
+
+router
+    .get('/', (ctx, next) => {
+        ctx.body = 'Hello World!';
+    })
+app
+    .use(router.routes())
+    .use(router.allowedMethods());
+
+router
+    .get('/koa_backend', (ctx, res) => {
+        ctx.body = ({ express: 'YOUR KOA BACKEND IS CONNECTED TO REACT' });
+});
+const server = app.listen(PORT, () => {
+    console.log(`Server listening on port: ${PORT}`);
+});
+
+module.exports = server;
