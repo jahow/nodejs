590103fa63251f98fd1ededb21e0ac7cd1de804d
julien.waddle@camptocamp.com <julien.waddle@camptocamp.com>
2018-12-11 15:57:18 +0100

    init config file for python scripts

---
+++ sig/scripts/config.py
@@ -0,0 +1,13 @@
+EPSG = '2154'
+PGPORT = 5432
+
+# from geogig.env
+PGDATABASE = 'gis'
+PGUSER = 'docker'
+PGPASSWORD = 'docker'
+PGSCHEMA = 'public'
+PGHOST= 'db'
+GSUSER = 'admin'
+GSPASSWORD = 'geoserver'
+
+GSREST = 'http://geoserver:8080/geoserver/rest/'
